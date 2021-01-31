efcd240fb7800283000ce5163c75059c0c58d087
Celia <celiahrrferreira@gmail.com>
2021-01-31 19:52:08 +0100

    DEVELOP: Change in custom validation

--- app/models/HNRValidation.java
+++ app/models/HNRValidation.java
@@ -6,13 +6,11 @@ import play.libs.F;
 public class HNRValidation extends Constraints.Validator<String> {
     @Override
     public F.Tuple<String, Object[]> getErrorMessageKey() {
-        System.out.println("aqui");
         return new F.Tuple<String, Object[]>(
                 "los productos son para reparación, nutrición o hidratación, en caso de no saberlo debe quedar sin indicar", new Object[]{""}); }
     @Override
     public boolean isValid(String value) {
-        System.out.println("aui tsmbine");
-        if (value.toLowerCase().equals("hidratación") || value.toLowerCase().equals("nutrición") || value.toLowerCase().equals("reparación") || value == null) {
+        if (value == null || value.toLowerCase().equals("hidratación") || value.toLowerCase().equals("nutrición") || value.toLowerCase().equals("reparación")) {
             return true;
         } else { return false;}
     }
